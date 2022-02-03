3b8a6d1aee56fa4c02f20fa23aa0d81cb2134630
Beau <halebeau3@gmail.com>
2022-02-02 17:11:27 -0800

    Working: on sightings controller

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -3,6 +3,11 @@ class SightingsController < ApplicationController
     sightings = Sighting.where(date: params[:start_date]..params[:end_date])
     render json: sightings
   end
+
+  def show
+      sightings = Sighting.find(params[:id])
+      render json: sightings
+  end
     
   def create
     sighting = Animal.find(params[:animal_id]).sightings.create(sighting_params)
@@ -37,3 +42,48 @@ private
         params.require(:sighting).permit(:date, :latitude, :longitude, :animal_id)
     end
 end
+
+
+
+# class SightingsController < ApplicationController
+
+#     def index
+#         sightings = Sighting.all
+#         render json:sightings
+#     end
+
+#     def show
+#         sightings = Sighting.find(params[:id])
+#         render json:sightings
+#     end
+
+#     def create
+#         sighting = Sighting.create(sighting_params)
+#         if sighting.valid?
+#             render json:sighting
+#         else
+#             render json:sighting.errors
+#         end
+#     end
+    
+#     def update 
+#         sighting = Sighting.find(params[:id])
+#         sighting.update(sighting_params)
+#         if sighting.valid?
+#             render json:sighting
+#         else
+#             render json:sighting.errors
+#         end
+#     end
+#     def destroy
+#         sighting = Sighting.find(params[:id])
+#         if sighting.destroy
+#             render json:sighting
+#         else
+#             render json:sighting.errors
+#         end
+#     end
+#     def sighting_params
+#         params.require(:sighting).permit(:animal_id, :date, :time, :latitude, :longitude)
+#     end
+# end
