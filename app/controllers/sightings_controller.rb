ff243b4d8c34b3ee2ca515d68380d2835d44fd07
Beau <halebeau3@gmail.com>
2022-02-02 16:08:04 -0800

    Added: sightingsg controller

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,45 +1,39 @@
 class SightingsController < ApplicationController
-  def index 
-    sightings = sightings.all
-    render json: sightings    
+  def index
+    sightings = Sighting.where(date: params[:start_date]..params[:end_date])
+    render json: sightings
   end
-end
-
-    def show
-        sighting = sighting.find(params[:id])
-        render json: sighting
-    end
     
-    def create
-        sighting = sighting.create(sighting_params)
-        if sighting.valid?
-            render json: sighting
-        else
-            render json: sighting.errors
-        end
+  def create
+    sighting = Animal.find(params[:animal_id]).sightings.create(sighting_params)
+    if sighting.valid?
+      render json: sighting
+    else
+      render json: sighting.errors, status: :unprocessable_entity
     end
+  end
 
-    def update
-        sighting = sighting.find(params[:id])
-        sighting.update(sighting_params)
-        if sighting.valid?
-            render json: sighting
-        else
-            render json: sighting.errors
-        end
+  def update
+    sighting = Animal.find(params[:animal_id]).sightings.find(params[:id])
+    sighting.update(sighting_params)
+    if sighting.valid?
+      render json: sighting
+    else
+      render json: sighting.errors
     end
+  end
 
-    def destroy
-        sighting = sighting.find(params[:id])
-        if sighting.destroy
-            render json: sighting
-        else
-            render json: sighting.errors 
-        end
+  def destroy
+    sighting = Animal.find(params[:animal_id]).sightings.find(params[:id])
+    if sighting.destroy
+      render json: sighting
+    else
+      render json: sighting.errors
     end
+  end
 
 private
     def sighting_params
-        params.require(:sighting).permit(:date, :latitude, :longitude)
+        params.require(:sighting).permit(:date, :latitude, :longitude, :animal_id)
     end
 end
